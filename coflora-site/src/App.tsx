455d81f4c8aa5dec86d6f4fae84dd45759091484
Saad <saadbch116@gmail.com>
2025-11-19 15:12:56 +0100

    Added navbar basics

--- coflora-site/src/App.tsx
+++ coflora-site/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Test from "./pages/HomePage";
 
 function App() {
-    return <h1>coflora</h1>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Test></Test>}></Route>
+                <Route path="*" element={<h1>404 Not Found</h1>}></Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
