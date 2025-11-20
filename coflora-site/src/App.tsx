dd1212fd17b81bfe17ee88242be5eea99d3609da
Nour Yassine <nour.saif.yassine@gmail.com>
2025-11-20 14:48:05 +0100

    add: about and App fix

--- coflora-site/src/App.tsx
+++ coflora-site/src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
+import About from "./pages/About";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage></HomePage>}></Route>
-                <Route path="/about"></Route>
+                <Route path="/about" element={<About></About>}></Route>
                 <Route path="/contact"></Route>
                 <Route path="/samples"></Route>
             </Routes>
---
+++ coflora-site/src/pages/About.tsx
@@ -0,0 +1,16 @@
+export default function About() {
+    return (
+        <section>
+            <div className="flex justify-center items-center">
+                <h2>Who and Why</h2>
+                <p>WHO?
+                    We are a team of like minded nature & art appreciators, that united to offer unique products to the world.
+
+                    We have expertise in Environmental Sciences, Sustainability, Climatology as well as fashion, design and arts. Here, Co-creating with nature, with its Flora, becomes possible, and brings all our knowledge and creativity in harmony, for the good of all.
+
+                    CoFlora brings together the care for nature and innovative design and art.
+                </p>
+            </div>
+        </section>
+    )
+}
