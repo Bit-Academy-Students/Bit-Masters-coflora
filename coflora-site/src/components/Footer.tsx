b9d985e8e20e0530d1b02e943bc3ca92cd5526b3
Nour Yassine <nour.saif.yassine@gmail.com>
2025-11-20 15:05:43 +0100

    add: Footer fix

--- coflora-site/src/components/Footer.tsx
+++ coflora-site/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
     return (
-        <footer className="flex items-center min-[320px]:flex-col flex-wrap min-[425px]:flex-row justify-evenly w-screen h-auto bg-amber-200">
+        <footer className="flex items-center min-[425px]:w-full min-[320px]:flex-col flex-wrap min-[425px]:flex-row justify-evenly w-screen h-auto bg-amber-200">
             <Link to="/" className="w-[280px] min-[425px]:w-[50vw] min-[435px]:w-[150px] aspect-video object-contain"><img src="https://coflora.org/wp-content/uploads/2022/08/coflora_original_black.png"></img></Link>
             <div className="container-contact relative min-[320px]:w-[250px] min-[320px]:py-7 text-sm">
                 <div className="py-2">Innovative Eco Solution for Interior, Exterior and events</div>
