c8714259224a7dbaf5774f6c1426eb800eda3bb3
Nour <nour.saif.yassine@gmail.com>
2025-11-27 15:24:08 +0100

    add: About

--- coflora-site/src/pages/About.tsx
+++ coflora-site/src/pages/About.tsx
@@ -1,15 +1,19 @@
 export default function About() {
     return (
-        <section>
-            <div className="flex justify-center items-center">
-                <h2>Who and Why</h2>
-                <p>WHO?
-                    We are a team of like minded nature & art appreciators, that united to offer unique products to the world.
+        <section className="w-screen overflow-x-hidden h-screen text-white">
+            <div className="relative flex w-full justify-evenly flex-wrap">
+                <img className="h-screen w-[200%] brightness-50 object-left object-cover" src="\src\assets\images\img.webp" alt="image" />
 
-                    We have expertise in Environmental Sciences, Sustainability, Climatology as well as fashion, design and arts. Here, Co-creating with nature, with its Flora, becomes possible, and brings all our knowledge and creativity in harmony, for the good of all.
+                <div className="flex flex-wrap flex-col absolute w-[50vw]">
+                    <h2 className="text-2xl">Who and Why</h2>
+                    <p className="text-[12px]">WHO?
+                        We are a team of like minded nature & art appreciators, that united to offer unique products to the world.
 
-                    CoFlora brings together the care for nature and innovative design and art.
-                </p>
+                        We have expertise in Environmental Sciences, Sustainability, Climatology as well as fashion, design and arts. Here, Co-creating with nature, with its Flora, becomes possible, and brings all our knowledge and creativity in harmony, for the good of all.
+
+                        CoFlora brings together the care for nature and innovative design and art.
+                    </p>
+                </div>
             </div>
         </section>
     )
