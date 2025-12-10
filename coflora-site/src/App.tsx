3db0a7908e81789d9e5c436550fa642dc7f7ee51
Nour Yassine <nour.saif.yassine@gmail.com>
2025-12-10 09:41:10 +0100

    add: Samples

--- coflora-site/src/App.tsx
+++ coflora-site/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
+import SamplesPage from "./pages/Samples";
 
 function App() {
     return (
@@ -9,7 +10,7 @@ function App() {
                 <Route path="/*" element={<HomePage></HomePage>}></Route>
                 <Route path="/about"></Route>
                 <Route path="/contact"></Route>
-                <Route path="/samples"></Route>
+                <Route path="/samples" element={<SamplesPage></SamplesPage>}></Route>
                 <Route path="*" element={<h1>404 Not Found</h1>}></Route>
             </Routes>
         </Router>
