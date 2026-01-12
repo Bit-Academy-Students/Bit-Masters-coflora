5c000199e293b131c85f728f859d0606a7885a0f
Nour <nour.saif.yassine@gmail.com>
2026-01-12 15:43:04 +0100

    fixes

--- coflora-site/src/pages/HomePage.tsx
+++ coflora-site/src/pages/HomePage.tsx
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
-import Banner from "../components/Banner";
-import Hero from "../components/Hero";
-import HomePageCard from "../components/HomePageCard";
-import { builder, designer } from "../assets/images";
-import { HomePageBuilderText, HomePageDesignerText } from "../constant";
+// import Banner from "../components/Banner";
+// import Hero from "../components/Hero";
+// import HomePageCard from "../components/HomePageCard";
 import Footer from "../components/Footer";
+// import { builder, designer } from "../assets/images";
+// import { HomePageBuilderText, HomePageDesignerText } from "../constant";
 
-function HomePage() {
+export default function HomePage() {
     return (
         <div className="w-full h-full">
-            <Hero />
+            {/* <Hero />
             <HomePageCard
                 name={"Builders"}
                 img={builder}
@@ -20,20 +19,8 @@ function HomePage() {
                 name={"Designers"}
                 img={designer}
                 text={HomePageDesignerText}
-            />
+            /> */}
             <Footer></Footer>
         </div>
     );
-=======
-import Footer from '../components/Footer.tsx';
-
-function HomePage() {
-    return (
-        <>
-            <Footer></Footer>
-        </>
-    )
->>>>>>> 1777beb8a4c964651547cecf9467e776a8c163c9
 }
-
-export default HomePage;
