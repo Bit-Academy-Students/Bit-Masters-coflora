ce32a2daa411a347224ac0761d2abcb2a6201d1e
Nour <nour.saif.yassine@gmail.com>
2026-01-12 10:51:18 +0100

    add: push

--- coflora-site/src/pages/About.tsx
+++ coflora-site/src/pages/About.tsx
@@ -1,6 +1,4 @@
 import Header from "../components/Header";
-import { img4 } from "../assets/images";
-import { img3 } from "../assets/images";
 // import woodenBg from '/wooden-background.jpg';
 
 export default function About() {
@@ -22,9 +20,9 @@ export default function About() {
                     <div className="rounded-full flex flex-wrap">
                         <img src="/wood.webp" className="w-[100px] h-[100px]" style={{ clipPath: "circle()" }} alt="wood" />
                         <img src="/wall.webp" className="w-[200px] h-[200px]" style={{ clipPath: "circle()" }} alt="phone" />
-                        <img src={img4} className="w-[150px] h-[150px]" style={{ clipPath: "circle()" }} alt="phone" />
-                        <img src={img3} className="w-[50px] h-[50px]" style={{ clipPath: "circle()" }} alt="" />
-                    </div>
+                            {/* <img src={img4} className="w-[150px] h-[150px]" style={{ clipPath: "circle()" }} alt="phone" />
+                            <img src={img3} className="w-[50px] h-[50px]" style={{ clipPath: "circle()" }} alt="" /> */}
+                        </div>
                 </div>
             </div>
         </section>
